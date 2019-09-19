c673412c89e112754e09e92d6aabea9ba3b96eaf
José Luiz Coe <joseluizcoe@gmail.com>
2019-09-18 21:55:49 -0300

    adicionando acao nos botoes para executar funcao que adiciona e remove elementos

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,17 @@
 // Write your Pizza Builder JavaScript in this file.
+
+function toggleElement(className) {
+  let elements = document.querySelectorAll(className);
+  elements.forEach((element) => {
+    element.classList.toggle('hidden');
+  });
+}
+
+let pepperButton = document.querySelector('.btn-green-peppers')
+pepperButton.onclick = () => toggleElement(".green-pepper");
+
+let pepperonniButton = document.querySelector('.btn-pepperonni')
+pepperonniButton.onclick = () => toggleElement(".pep");
+
+let mushroomButton = document.querySelector('.btn-mushrooms')
+mushroomButton.onclick = () => toggleElement(".mushroom");
